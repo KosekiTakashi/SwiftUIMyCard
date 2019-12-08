eeaf5722342d157b6a42ea8d645e87d8a9b290a9
KosekiTakashi <takashi111398@gmail.com>
2019-12-08 12:25:03 +0900

    Initial Commit

---
+++ KosekiCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KosekiCard
+//
+//  Created by 小関隆司 on 2019/12/08.
+//  Copyright © 2019 kosekitakashi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
